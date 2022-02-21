fc51e72565145ab34644ddac4852128862aff50d
Christoph Timm <quaquaro@gmail.com>
2022-02-21 13:33:05 +0100

    Fix 2 of three tests

--- src/pages/Login.spec.js
+++ src/pages/Login.spec.js
@@ -1,18 +1,21 @@
 import LoginPage from './LoginPage.js';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
+import { MemoryRouter } from 'react-router-dom';
 
 describe('LoginPage', () => {
   it('renders a hidden paragraph, a headline , a input and a button', () => {
-    render(<LoginPage />);
+    render(
+      <MemoryRouter>
+        <LoginPage />
+      </MemoryRouter>
+    );
 
     // const allFormElements = screen.getByRole('');
     const input = screen.getByRole('textbox');
     const button = screen.getByRole('button');
     const heading = screen.getByRole('heading');
     // const paragraph = screen.getByText(/user/i);
-    
 
     expect(input).toBeInTheDocument();
     expect(input).toHaveAttribute('placeholder', 'Username e.g.');
@@ -24,7 +27,11 @@ describe('LoginPage', () => {
 
   it('Click on Button submits the input value to the spacetraders api', () => {
     const handleLogin = jest.fn();
-    render(<LoginPage onLogin={handleLogin} />);
+    render(
+      <MemoryRouter>
+        <LoginPage onLogin={handleLogin} />
+      </MemoryRouter>
+    );
 
     const input = screen.getByRole('textbox');
     const submitButton = screen.getByRole('button');
@@ -33,22 +40,23 @@ describe('LoginPage', () => {
     userEvent.type(input, 'Hennifant');
     userEvent.click(submitButton);
 
-    expect(handleLogin).toHaveBeenCalledWith(
-      'Hennifant'
-    );
-  })
-
-    it('does not submit form if the input is empty', () => {
-      const handleSubmit = jest.fn();
-      render(<LoginPage onSubmit={handleSubmit} />);
-      
-      const input = screen.getByRole('textbox');
-      const submitButton = screen.getByRole('button');
-
-      userEvent.type(input, '');
-      userEvent.click(submitButton);
-
-      expect(handleSubmit).not.toHaveBeenCalled();
-    });
+    expect(handleLogin).toHaveBeenCalledWith('Hennifant');
   });
 
+  it('does not submit form if the input is empty', () => {
+    const handleSubmit = jest.fn();
+    render(
+      <MemoryRouter>
+        <LoginPage onSubmit={handleSubmit} />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByRole('textbox');
+    const submitButton = screen.getByRole('button');
+
+    userEvent.type(input, 'asd');
+    userEvent.click(submitButton);
+
+    expect(handleSubmit).not.toHaveBeenCalled();
+  });
+});
